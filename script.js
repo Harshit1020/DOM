084d279717a34726922723a13d764d9b47aeb061
Harshit_Dongre <95414266+Harshit1020@users.noreply.github.com>
2024-01-15 14:34:35 +0530

    imag card mouse effect

--- script.js
+++ script.js
@@ -39,15 +39,42 @@
 // })
 
 
-var main = document.querySelector("#main")
-var crsr = document.querySelector(".crsr")
+// var main = document.querySelector("#main")
+// var crsr = document.querySelector(".crsr")
 
 
-main.addEventListener("mousemove", function(dets){
+// main.addEventListener("mousemove", function(dets){
 
-    crsr.style.left = dets.x + "px";
-    crsr.style.top = dets.y + "px";
+//     crsr.style.left = dets.x + "px";
+//     crsr.style.top = dets.y + "px";
     
+// })
 
+// mouser hover image effect
+var elem = document.querySelectorAll(".elem");
+
+elem.forEach((val)=>{
+
+    // console.log(val)
+  val.addEventListener("mouseenter",function(){
+
+    val.childNodes[3].style.opacity = 1;
+
+  })  
+
+
+  val.addEventListener("mouseleave",function(){
+
+    val.childNodes[3].style.opacity = 0;
+
+  })
+
+
+  
+  val.addEventListener("mousemove",function(dets){
+    val.childNodes[3].style.left= dets.x + "px";
+    val.childNodes[3].style.top = dets.y + "px";
+  })
+    
+    
 })
-
