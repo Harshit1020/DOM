f26222114e1e8e05479c44a83b317409c26065e9
Harshit_Dongre <95414266+Harshit1020@users.noreply.github.com>
2024-01-15 11:47:34 +0530

    love effect

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-let txt = document.querySelector("h5");
-let addbtn = document.querySelector("#add");
-var flg = 0;
+// let txt = document.querySelector("h5");
+// let addbtn = document.querySelector("#add");
+// var flg = 0;
 
-addbtn.addEventListener("click" , function(){
+// addbtn.addEventListener("click" , function(){
 
-    if(flg == 0){
-        txt.innerHTML = "Friends"
-        txt.style.color = "green"
-        addbtn.innerHTML = "Remove"
-        flg = 1;
-    }
-    else{
+//     if(flg == 0){
+//         txt.innerHTML = "Friends"
+//         txt.style.color = "green"
+//         addbtn.innerHTML = "Remove"
+//         flg = 1;
+//     }
+//     else{
 
-        txt.innerHTML = "Strangers"
-        txt.style.color = "black"
-        addbtn.innerHTML = "Add"
-        flg = 0;
+//         txt.innerHTML = "Strangers"
+//         txt.style.color = "black"
+//         addbtn.innerHTML = "Add"
+//         flg = 0;
 
-    }
+//     }
+// })
+
+
+// Insta Love effect...................
+let card = document.querySelector("#card")
+let icon = document.querySelector("i")
+
+card.addEventListener("dblclick", function(){
+
+    icon.style.transform = "translate(-50%,-50%) scale(1)"
+    icon.style.color = "red"
+    icon.style.opacity = 0.8
+    setTimeout(()=>{
+        icon.style.opacity = 0
+
+    },1000)
+    setTimeout(()=>{
+        icon.style.transform = "translate(-50%,-50%) scale(0)"
+
+    },3000)
+
 })
 
